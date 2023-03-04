7e267705a67b5be744a58799ab4ecb77508fe153
Joohan Lee <joohan33@gmail.com>
2023-03-04 18:58:49 -0500

    amend to the last commit

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,1 +1,35 @@
 package arrays
+
+func SumAll(numsOfNums ...[]int) []int {
+	//result := make([]int, len(numsOfNums))
+	var result []int
+
+	for _, nums := range numsOfNums {
+		result = append(result, Sum(nums))
+	}
+
+	return result
+}
+
+func Sum(nums []int) int {
+	result := 0
+	for _, num := range nums {
+		result += num
+	}
+
+	return result
+}
+
+func SumAllTails(numsOfNums ...[]int) []int {
+	var result []int
+	for _, nums := range numsOfNums {
+		if len(nums) < 1 {
+			result = append(result, 0)
+		} else {
+			result = append(result, Sum(nums[1:]))
+		}
+
+	}
+
+	return result
+}
